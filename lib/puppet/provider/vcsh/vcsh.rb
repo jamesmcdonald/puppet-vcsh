3e2c3f0ecb696e04ac829309244cb421ba23f808
James McDonald <james.mcdonald@spk.no>
2016-12-06 09:09:53 +0100

    Apply changes recommended by rubocop

--- lib/puppet/provider/vcsh/vcsh.rb
+++ lib/puppet/provider/vcsh/vcsh.rb
@@ -1,23 +1,26 @@
 Puppet::Type.type(:vcsh).provide(:vcsh) do
-  desc "Implement vcsh repo goo"
+  desc 'Implement vcsh repo goo'
 
   def create
-    puts "#{@resource[:name]} #{self.repo}"
-    puts `su #{@resource[:user]} -c "vcsh clone #{@resource.should(:repo)} #{@resource[:name]}"`
+    puts "#{@resource[:name]} #{repo}"
+    puts `su #{@resource[:user]} -c
+      "vcsh clone #{@resource.should(:repo)} #{@resource[:name]}"`
   end
 
   def destroy
-    false 
+    false
   end
 
   def exists?
-    `su #{@resource[:user]} -c "vcsh list"`.split.include? @resource[:name]
+    `su #{@resource[:user]} -c
+      "vcsh list"`.split.include? @resource[:name]
   end
 
   def repo
     # This is messy to get something working quickly
-    if self.exists?
-      `su #{@resource[:user]} -c "vcsh #{@resource[:name]} remote -v"`.lines[0].split[1]
+    if exists?
+      `su #{@resource[:user]} -c
+        "vcsh #{@resource[:name]} remote -v"`.lines[0].split[1]
     else
       :absent
     end
--- lib/puppet/type/vcsh.rb
+++ lib/puppet/type/vcsh.rb
@@ -1,26 +1,24 @@
 Puppet::Type.newtype(:vcsh) do
-  @doc = "Manage vcsh repo"
+  @doc = 'Manage vcsh repo'
   ensurable
 
   newparam(:name) do
-    desc "The name of the vcsh repo"
+    desc 'The name of the vcsh repo'
   end
 
   newparam(:user) do
-    desc "The user to clone the repo as"
+    desc 'The user to clone the repo as'
   end
 
   newproperty(:repo) do
-    desc "The git URL of the repo to clone"
+    desc 'The git URL of the repo to clone'
   end
 
   newproperty(:branch) do
-    desc "The name of the branch to use"
+    desc 'The name of the branch to use'
   end
 
   validate do
-    unless self[:user]
-      raise(Puppet::Error, "user is a required attribute")
-    end
+    raise(Puppet::Error, 'user is a required attribute') unless self[:user]
   end
 end
