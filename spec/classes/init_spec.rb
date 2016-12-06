732c543493b30c9fd9ec54773438a4bc2b97ee20
James McDonald <james.mcdonald@spk.no>
2016-12-06 09:07:48 +0100

    Add a test for the class

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -2,5 +2,7 @@ require 'spec_helper'
 describe 'vcsh' do
   context 'with default values for all parameters' do
     it { should contain_class('vcsh') }
+    it { should contain_package('vcsh') }
+    it { should contain_package('git') }
   end
 end
